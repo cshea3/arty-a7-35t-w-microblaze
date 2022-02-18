623baf6adfd0ad1db1c9f9f111aaac6291318528
Colin William Shea <cshea3@umbc.edu>
2022-02-18 11:56:52 -0500

    Added in the base PWM software from the workspace. Also included a .gitignore file to remove build artifacts that do not need to be included.

---
+++ sw/xtmrctr_pwm_example_1/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LD_SRCS += \
+../src/lscript.ld 
+
+C_SRCS += \
+../src/xtmrctr_pwm_example.c 
+
+OBJS += \
+./src/xtmrctr_pwm_example.o 
+
+C_DEPS += \
+./src/xtmrctr_pwm_example.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MicroBlaze gcc compiler'
+	mb-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -I/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include -mlittle-endian -mxl-pattern-compare -mcpu=v11.0 -mno-xl-soft-mul -Wl,--no-relax -ffunction-sections -fdata-sections -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ sw/xtmrctr_pwm_example_1/Debug/src/xtmrctr_pwm_example.d
@@ -0,0 +1,47 @@
+src/xtmrctr_pwm_example.o src/xtmrctr_pwm_example.o: \
+ ../src/xtmrctr_pwm_example.c \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xtmrctr.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_types.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_assert.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xstatus.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xtmrctr_l.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_io.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_printf.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xparameters.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/bspconfig.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/mb_interface.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_exception.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xparameters.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_exception.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xintc.h \
+ /home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xintc_l.h
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xtmrctr.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_types.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_assert.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xstatus.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xtmrctr_l.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_io.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_printf.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xparameters.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/bspconfig.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/mb_interface.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_exception.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xparameters.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_exception.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xintc.h:
+
+/home/cwshea/workspace_2020_2/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xintc_l.h:
